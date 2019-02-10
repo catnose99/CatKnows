7451b12067ca85ff5201c02a69849d271a6f2c98
catnose-onair <nodding.com@gmail.com>
2019-02-10 15:02:41 +0900

    fix: bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`
+      twitter: `catnose99`
     }
   },
   plugins: [
