aefe822d1ae942282ae1bc353497e826e9118594
catnose-onair <nodding.com@gmail.com>
2019-02-20 12:46:14 +0900

    add: image block

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,9 @@ module.exports = {
                 notice: {
                   classes: "notice",
                   title: "optional"
+                },
+                image: {
+                  classes: "image"
                 }
               }
             }
